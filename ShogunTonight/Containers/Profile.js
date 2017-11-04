eba9f66a9a66cc0c260b07c4ee621508197bbbce
julien athomas <web.nestenn@gmail.com>
2017-11-04 10:46:24 +0100

    add picker

--- ShogunTonight/Containers/Profile.js
+++ ShogunTonight/Containers/Profile.js
@@ -12,22 +12,70 @@ import {
   View,
   AppRegistry,
   Actions,
-  TouchableOpacity
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
-import ImagePicker from 'react-native-image-crop-picker';
+var ImagePicker = require('react-native-image-picker');
+
+// More info on all the options is below in the README...just some common use cases shown here
+var options = {
+  title: 'Select Avatar',
+  customButtons: [
+    {name: 'fb', title: 'Choose Photo from Facebook'},
+  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
+
 
 export default class Profile extends Component<{}> {
   _onPressButton() {
    Actions.events();
 
- }
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      profileImg: null
+    };
+  }
+
+  onImageClicked() {
+    ImagePicker.showImagePicker(null, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        this.setState({
+          profileImg: source
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <View>
         <View style={{marginTop: 150, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity onPress={this._onPressButton}>
-                       <Text>Photo</Text>
+        <TouchableOpacity onPress={this.onImageClicked.bind(this)}>
+          <Text>Photo</Text>
+          {this.state.profileImg != null &&
+            <Image style={{width: 200, height: 200}} source={this.state.profileImg} />
+          }
          </TouchableOpacity>
         </View>
      </View>
